f74f780afb3149704f9c06ce61773b1ef65dcba0
SeaStove <rochovian@gmail.com>
2023-07-21 17:11:56 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
+require("dotenv").config();
 const express = require("express");
+const needle = require("needle");
+const url = require("url");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type("html").send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/api/health", (req, res) => res.send("OK"));
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+async function commonGet(subroute, req) {
+  const params = new URLSearchParams({
+    ...url.parse(req.url, true).query,
+    [API_KEY_NAME]: API_KEY_VALUE,
+  });
+
+  const requestUrl = `${API_BASE_URL}${subroute}?${params}`;
+  console.log(requestUrl);
+  const apiRes = await needle("get", requestUrl);
+  const data = apiRes.body;
+
+  return data;
+}
+
+app.get("/api/search/movies", async (req, res, next) => {
+  try {
+    const data = await commonGet("/search/movie", req);
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/movie/:movieId/credits", async (req, res, next) => {
+  try {
+    const data = await commonGet(`/movie/${req.params.movieId}/credits`, req);
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -58,4 +100,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
